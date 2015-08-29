770402b27375f3eda3115cd2ae1e458d1d96e2f8
Andrew Steele <andrew@andrewsteele.me.uk>
2015-08-29 10:15:18 +0100

    Fixed errors with CellRendererText example

--- _examples/cellrenderertext.c
+++ _examples/cellrenderertext.c
@@ -5,48 +5,60 @@ static void destroy(GtkWidget *widget, gpointer data)
     gtk_main_quit();
 }
 
+static void on_cell_edited(GtkWidget *cellrenderer, gchar *path, gchar *text, gpointer data)
+{
+    GtkTreeModel *model = (GtkTreeModel *)data;
+    GtkTreePath *treepath = gtk_tree_path_new_from_string(path);
+    GtkTreeIter treeiter;
+
+    gtk_tree_model_get_iter(model, &treeiter, treepath);
+
+    gtk_list_store_set(GTK_LIST_STORE(model), &treeiter, 1, text, -1);
+}
+
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
-    
+
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
-    
+
     GtkListStore *liststore = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
     GtkTreeIter treeiter;
-    
+
     gtk_list_store_append(liststore, &treeiter);
-    gtk_list_store_set(liststore, &treeiter, 0, "Zenwalk", 1, "http://www.zenwalk.org/", -1);
+    gtk_list_store_set(liststore, &treeiter, 0, "Potatoes", 1, "4", -1);
     gtk_list_store_append(liststore, &treeiter);
-    gtk_list_store_set(liststore, &treeiter, 0, "CentOS", 1, "http://www.centos.org/", -1);
+    gtk_list_store_set(liststore, &treeiter, 0, "Tissues", 1, "1", -1);
     gtk_list_store_append(liststore, &treeiter);
-    gtk_list_store_set(liststore, &treeiter, 0, "Ubuntu", 1, "http://www.ubuntu.com/", -1);
+    gtk_list_store_set(liststore, &treeiter, 0, "Yoghurts", 1, "3", -1);
     gtk_list_store_append(liststore, &treeiter);
-    gtk_list_store_set(liststore, &treeiter, 0, "Puppy", 1, "http://www.puppylinux.org/", -1);
+    gtk_list_store_set(liststore, &treeiter, 0, "Milk", 1, "2", -1);
     gtk_list_store_append(liststore, &treeiter);
-    gtk_list_store_set(liststore, &treeiter, 0, "Gentoo", 1, "http://www.gentoo.org/", -1);
-    
+    gtk_list_store_set(liststore, &treeiter, 0, "Soup", 1, "3", -1);
+
     GtkWidget *treeview = gtk_tree_view_new_with_model(GTK_TREE_MODEL(liststore));
     gtk_container_add(GTK_CONTAINER(window), treeview);
-    
+
     GtkTreeViewColumn *treeviewcolumn;
     GtkCellRenderer *cellrenderertext;
-    
+
     cellrenderertext = gtk_cell_renderer_text_new();
-    treeviewcolumn = gtk_tree_view_column_new_with_attributes("Distribution",
+    treeviewcolumn = gtk_tree_view_column_new_with_attributes("Grocery",
                                                               cellrenderertext,
-                                                              "text", 0, NULL);
+                                                              "text", 0,
+                                                              NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), treeviewcolumn);
-    cellrenderertext = gtk_cell_renderer_text_new();
-    gtk_widget_set_property(GTK_WIDGET(cellrenderertext), "editable", TRUE);
-    treeviewcolumn = gtk_tree_view_column_new_with_attributes("Website",
+    treeviewcolumn = gtk_tree_view_column_new_with_attributes("Quantity",
                                                               cellrenderertext,
                                                               "text", 1, NULL);
+    g_object_set(cellrenderertext, "editable", TRUE, NULL);
+    g_signal_connect(cellrenderertext, "edited", G_CALLBACK(on_cell_edited), liststore);
     gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), treeviewcolumn);
-    
+
     gtk_widget_show_all(window);
-    
+
     gtk_main();
-    
+
     return 0;
 }
